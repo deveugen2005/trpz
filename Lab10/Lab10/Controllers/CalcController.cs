43b7619bb88c96347448310862de2f080a6fff38
deveugen2005 <deveugen2005@gmail.com>
2024-12-19 15:40:00 +0200

    Add 10 lab

---
+++ Lab10/Lab10/Controllers/CalcController.cs
@@ -0,0 +1,33 @@
+﻿using Lab10.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Globalization;
+
+namespace Lab10.Controllers
+{
+    public class CalcController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(OperandsModel model)
+        {
+            var cleanedX = model.X.Replace(",", ".");
+            double x;
+            if (double.TryParse(cleanedX, NumberStyles.Any, CultureInfo.InvariantCulture, out x))
+            {
+                var result = 5 * Math.Pow((x - 3), 6) - 4 * Math.Pow((x - 8), 4) + 42;
+                ViewBag.Result = $"Значення функції: {result}";
+            }
+            else
+            {
+                ViewBag.Result = "Помилка: введене значення некоректне.";
+            }
+
+            return View();
+        }
+    }
+}
---
+++ Lab10/Lab10/Models/OperandsModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Lab10.Models
+{
+    public class OperandsModel
+    {
+        public string X { get; set; }
+        public double Y { get; set; }
+    }
+}
