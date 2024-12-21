936061d17a7a6d9c356387e57e61ee4609f84796
deveugen2005 <deveugen2005@gmail.com>
2024-12-21 23:46:32 +0200

    update Lab12

---
+++ Lab12/Lab12/App_Start/WebApiConfig.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.UI.WebControls;
+
+namespace Lab12.App_Start
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            config.Routes.MapHttpRoute(
+                name: "WApi",
+                routeTemplate: "api/{controller}/{action}/{id}",
+                defaults: new { id = System.Web.Http.RouteParameter.Optional }
+            );
+
+            config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new System.Net.Http.Headers.MediaTypeHeaderValue("text/html"));
+        }
+    }
+}
